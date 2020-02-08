2f3783b61b56007e33b9783fb59d11a81fb5848d
nayanika-g <47043708+nayanika-g@users.noreply.github.com>
2020-02-08 12:02:29 -0800

    Update euler.m

--- euler.m
+++ euler.m
@@ -1,9 +1,9 @@
 clear; clc
-h = 0.1; %edit this
-t_final = 4; %edit this
+h = 0.1; 
+t_final = 4; 
 N = t_final/h;
 
-%Initial condition %edit these
+%Initial condition
 t(1) = 0; 
 y(1) = 1;
 v(1) = 0;
@@ -12,11 +12,11 @@ v(1) = 0;
 for i=1:N
     t(i+1) = t(i)+h;
     y(i+1) = y(i)+h*v(i);
-    v(i+1) = v(i)+h*(-4*y(i)); %?
+    v(i+1) = v(i)+h*(-4*y(i));
 end
 
 plot(t,y);
 xlabel('t');
 ylabel('y');
-xlim([0 4]); %edit this
+xlim([0 4]); 
 %ylim([-1 1]);
